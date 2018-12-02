aabdc172163251a424b7ca583e9feae563f614a8
Jbellard92 <36765066+Jbellard92@users.noreply.github.com>
2018-12-01 19:14:33 -0500

    Mission to mars master file

---
+++ missiontomars/app.py
@@ -0,0 +1,25 @@
+# import necessary libraries
+from flask import Flask, render_template
+import pymongo
+import scrape
+
+app = Flask(__name__)
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient()
+db = client.mars_db
+collection = db.mars_data_entries
+
+@app.route("/")
+def home():
+    mars_data = list(db.collection.find())[0]
+    return  render_template('index.html', mars_data=mars_data)
+
+@app.route("/scrape")
+def web_scrape():
+    db.collection.remove({})
+    mars_data = scrape.scrape()
+    db.collection.insert_one(mars_data)
+    return  render_template('scrape.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
